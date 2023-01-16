03ea4e8da06812365a0b69456e1696786cf878c3
Thore <mail@thoreeichhorn.de>
2023-01-16 19:59:40 +0100

    Refactored

--- src/s2_a2.py
+++ src/s2_a2.py
@@ -2,4 +2,3 @@ import re
 
 if __name__ == "__main__":
     print(re.sub("the the", "the", "If the the problem is textual, use the the re module."))
-
--- src/s2_a4_b.py
+++ src/s2_a4_b.py
@@ -1,32 +1,30 @@
 import unittest
-import re
-import s2_a4_a as numParser
+import s2_a4_a as num_parser
 
 
-class testNumbers(unittest.TestCase):
-    numbers = ["+1 223-456 7890", "1-223-456-7890", "+1 223 456-7890", "(223) 456-7890", "1 223 456 7890",
-               "223.456.7890"]
+class TestNumbers(unittest.TestCase):
+    numbers = ["+1 223-456 7890", "1-223-456-7890", "+1 223 456-7890", "(223) 456-7890", "1 223 456 7890", "223.456.7890"]
 
     def test_number_array(self):
         for i in range(0, len(self.numbers) - 1):
-            self.assertEqual(numParser.parseNumber(self.numbers[i]), '1-223-456-7890')
+            self.assertEqual(num_parser.parse_number(self.numbers[i]), '1-223-456-7890')
 
     def test_to_short_number(self):
-        with self.assertRaises(numParser.ValueError):
-            numParser.parseNumber("1-223-456-789")
+        with self.assertRaises(num_parser.ValueError):
+            num_parser.parse_number("1-223-456-789")
 
     def test_to_long_number(self):
-        with self.assertRaises(numParser.ValueError):
-            numParser.parseNumber("1-223-456-78999")
+        with self.assertRaises(num_parser.ValueError):
+            num_parser.parse_number("1-223-456-78999")
 
     def test_false_prefix(self):
-        with self.assertRaises(numParser.ValueError):
-            numParser.parseNumber("+2 223-456-7899")
+        with self.assertRaises(num_parser.ValueError):
+            num_parser.parse_number("+2 223-456-7899")
 
-    def test_false_first_ortsvorwhal(self):
-        with self.assertRaises(numParser.ValueError):
-            numParser.parseNumber("+1 123-456-7899")
+    def test_false_first_ortsvorwahl(self):
+        with self.assertRaises(num_parser.ValueError):
+            num_parser.parse_number("+1 123-456-7899")
 
-    def test_false_second_ortsvorwhal(self):
-        with self.assertRaises(numParser.ValueError):
-            numParser.parseNumber("+1 193-456-7899")
+    def test_false_second_ortsvorwahl(self):
+        with self.assertRaises(num_parser.ValueError):
+            num_parser.parse_number("+1 193-456-7899")
