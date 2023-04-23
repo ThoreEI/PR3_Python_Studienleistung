9f7a26039dcf21b4b3d03529cf16fa99a6237479
Thore <mail@thoreeichhorn.de>
2023-04-23 03:53:58 +0200

    Refactored

--- src/s2_3a.py
+++ src/s2_3a.py
@@ -1,52 +1,46 @@
 import json
+import pprint
 import re
 
 if __name__ == '__main__':
     with open("../docs/Personen.txt", "r") as input_file:
-        personal_data = input_file.readlines()
-    input_file.close()
-
-    list_of_persons = [None] * len(personal_data)
-    index = 1
-    for read_line in personal_data:
+        data = input_file.readlines()
+    persons = []
+    for index, line in enumerate(data):
         try:
-            personal_data = re.split(",", read_line)
-            full_name = re.split("\s", personal_data[0])
-            last_name = full_name.pop(-1)
-            title = second_name = ""
-            while re.match('Prof\.|Dr\.|Univ\.|Ing\.|Dipl\.', full_name[0]):
-                title += full_name.pop(0)
-            title = None if title == "" else title
-            first_name = full_name.pop(0)
-            while len(full_name) > 0:
-                second_name = full_name.pop(0)
-            second_name = None if second_name == "" else second_name
-
-            street_and_number = personal_data[1]
-            street_name = "".join(re.findall("\D", street_and_number)).replace("\"", "")
-            street_number = "".join(re.findall(r"\b\d{1,4}\b", street_and_number))
-
-            postal_code_and_residence = personal_data[2]
-            postal_code = "".join(re.findall("\d{5}", postal_code_and_residence))
-            residence = "".join(re.findall("\D", postal_code_and_residence)).replace("\"", "")
-
-            birthdate = "".join(personal_data[3]).replace("-", ".")
-            phone_number = personal_data[4].replace("\n", "")
+            info = line.strip().replace("\"", "").split(",")
+            name_parts = info[0].split(" ")
+            last_name = name_parts.pop(-1)
+            title = ""
+            if re.match("(Dr.|Dipl.|-?Ing.|Prof.|Univ.)\s*", name_parts[0]):
+                title = " ".join(re.findall("(Dr.|Dipl.|-?Ing.|Prof.|Univ.)\s*", name_parts.pop(0)))
+            first_name = name_parts.pop(0)
+            # middle_name = " ".join(re.findall(r"[A-Z][a-z]+(-?[A-Z][a-z]+)?\s", name_parts[0]))
+            middle_name = ""
+            while len(name_parts) > 0:
+                middle_name += name_parts.pop(0)
+            street_name = "".join(re.findall("\D", info[1]))
+            street_number = "".join(re.findall(r"\b\d{1,5}\b", info[1]))
+            postal_code = "".join(re.findall("\d{5}", info[2]))
+            residence = "".join(re.findall("\D", info[2]))
+            birthdate = "".join(info[3]).replace("-", ".")
+            phone_number = info[4]
             person = {
-                "Index": index,
-                "Titel": [title],
+                "Index": [index],
+                "Titel": [None if title == "" else title],
                 "Vorname": [first_name],
-                "Zweitname": [second_name],
+                "Zweitname": [None if middle_name == "" else middle_name],
                 "Nachname": [last_name],
                 "Strasse": [street_name],
                 "Hausnummer": [street_number],
                 "PLZ": [postal_code],
                 "Wohnort": [residence],
-                "Geburtsdatum": [birthdate]
+                "Geburtsdatum": [birthdate],
+                "Telefon": [phone_number]
             }
-            list_of_persons[index - 1] = person
-            index += 1
+            persons.append(person)
         except IndexError:
             continue
+        pprint.pp(persons)
     with open(r"Personen_Neu.json", "w") as output_file:
-        json.dump(list_of_persons, output_file, indent=1, ensure_ascii=False)
+        json.dump(persons, output_file, indent=1, ensure_ascii=False)
--- src/s2_a1.py
+++ src/s2_a1.py
@@ -1,19 +1,19 @@
 class Vehicle:
     def __init__(self, tires) -> None:
-        self.__tires: int = tires
+        self._tires: int = tires
 
 
 class Car(Vehicle):
     def __init__(self, engine):
         super().__init__(tires=4)
-        self.__engine: str = engine
+        self._engine: str = engine
 
 
-class BondCar(Vehicle, Car):
-    def __init__(self, max_speed: float):
+class SportCar(Vehicle, Car):
+    def __init__(self, max_speed):
         super().__init__(engine="Fast")
-        self.__max_speed = max_speed
+        self._max_speed: float = max_speed
 
 
 if __name__ == "__main__":
-    bond_car = BondCar(123.45)
+    porsche_911 = SportCar(max_speed=280.45)
